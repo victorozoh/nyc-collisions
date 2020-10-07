01919ae6dd47dd8a1f2f4b39ecbcca8a015e379d
Victor Ozoh <bbgenie@gmail.com>
2020-10-07 15:28:07 -0500

    added map to index.html

---
+++ app/static/js/display_locations.js
@@ -0,0 +1,19 @@
+var borough;
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmJtYXBib3giLCJhIjoiY2tmenRyeWcyMmR3eDJ5czNzcjd0MG1sMiJ9.TYbeYuIfPsb178PAPzUj0w';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v9', // stylesheet location
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9 // starting zoom
+})
+
+// display locations on a map
+$(function(){
+
+  $("#display").click(function(){
+    borough = $( "#selected_borough" ).val();
+    console.log(borough);
+  });
+
+});
