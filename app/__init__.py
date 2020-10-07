2af51eee5111d37b5ca36d3984d3230b121faefd
Victor Ozoh <bbgenie@gmail.com>
2020-10-07 13:49:43 -0500

    edits to api routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from config import Config
+import psycopg2
 
+
+application = Flask(__name__)
+# configure the Flask app
+application.config.from_object(Config)
+DB_URI = application.config['POSTGRESQL_URI']
+
+# Get Heroku database connection
+connection = psycopg2.connect(DB_URI)
+
+# register routes
 from .api.routes import api
 from .home.routes import homepage
-
-def create_app():
-    app = Flask(__name__)
-
-    app.register_blueprint(api)
-    app.register_blueprint(homepage)
-
-    return app
+application.register_blueprint(api)
+application.register_blueprint(homepage)
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,7 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from .. import connection
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
-@api.route('/boroughs/<string:borough>')
+def fetch_records(query, field):
+    """
+    args: query(SQL statement as a string ), field(filter for SQL statement)
+    returns: JSON object
+    This function returns a JSON object of all records matching the
+    SQL query statement
+    """
+    records = None
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query, (field,))
+            records = cursor.fetchall()
+    return jsonify(records)
+
+
+@api.route('/boroughs/<string:borough>', methods=['GET'])
 def boroughs(borough):
-    return {'one' : borough}
+    select_query = "select latitude, longitude from collisions where borough = %s"
+    records = None
+    fetch_records(select_query, borough)
+
+
+@api.route('/zipcodes/<string:zipcode>', methods=['GET'])
+def zipcodes(zipcode):
+    select_query = "select latitude, longitude from collisions where zipcode = %s"
+    records = None
+    fetch_records(select_query, zipcode)
